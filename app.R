3706ad7563544baea78db6a5db76ed2b75cb25de
Altay Yuzeir <34285813+AltayYuzeir@users.noreply.github.com>
2022-09-08 16:05:38 +0200

    Update app.R

--- app.R
+++ app.R
@@ -60,6 +60,7 @@ ui = fluidPage(
                            span(style = "color:#cc6699;font-weight:bold;",
                                 dataTableOutput("Database")
                            ),
+                           
                            icon = span(style = "color:#cc6699;font-weight:bold;",
                                        icon("table"))
                   )
@@ -138,8 +139,51 @@ server = function(input, output, session) {
     shinyjs::hide("showhideMasterPasswordField")
   })
   
+  # Reload button ----
   observeEvent(input$reloadButton, {
-    session$reload()
+    output$Database = renderDataTable({
+      new_datatable = matrix(nrow = 1, ncol = 2)
+      new_datatable = data.frame(new_datatable)
+      colnames(new_datatable) = c("Website", "Login")
+      MasterPassword = input$masterPassword
+      if(is_empty_input(MasterPassword))
+      {
+        new_datatable$Website[1] = "Please provide"
+        new_datatable$Login[1] = "Master password !"
+        new_datatable
+      } else{
+        
+        passphrase <- charToRaw(MasterPassword)
+        key <- openssl::sha256(passphrase)
+        if(file.exists(paste0(path,"Database.ycpt"))){
+          database = read.aes(paste0(path,"Database.ycpt"), key = key)
+          if( !all(colnames(database) == c("Website", "Login", "Password") ) )
+          {
+            new_datatable$Website[1] = "Please provide correct"
+            new_datatable$Login[1] = "Master password !"
+            new_datatable
+          } else {
+            if(nrow(database) == 1)
+            {
+              new_datatable$Website[1] = "No records exist"
+              new_datatable$Login[1] = "in the Database !"
+              new_datatable
+            } else{
+              new_datatable2 = database$Website[2:length(database$Website)]
+              new_datatable3 = database$Login[2:length(database$Login)]
+              new_datatable = as.data.frame(cbind(new_datatable2,new_datatable3))
+              colnames(new_datatable) = c("Website", "Login")
+              new_datatable
+            }
+          }
+        } else
+        {
+          new_datatable$Website[1] = "Encrypted Database"
+          new_datatable$Login[1] = "does not exist !"
+          new_datatable
+        }
+      }
+    })
   })
   
   #### Show-Hide Password ----
